b0202c6fea3091343daf775f8722ad6e969e6882
shlok <shloksomani@ymail.com>
2019-07-25 20:07:59 -0400

    added touch support for touch devices

--- script.js
+++ script.js
@@ -2,29 +2,36 @@ const face = document.getElementsByClassName("face")[0];
 const jokeBox = document.getElementById("joke");
 const curtains = Array.from(document.getElementsByClassName("face-bar"));
 
-window.addEventListener("keypress", e => {
-  e.preventDefault();
-  if (e.keyCode === 32) {
-    // Shut
-    curtains.forEach(bar => {
-      bar.classList.add("shut");
-      bar.classList.remove("open");
-    });
-    // Fetch
-    window.setTimeout(function() {
-      fetch("https://icanhazdadjoke.com/", {
-        headers: { Accept: "application/json" }
-      })
-        .then(res => res.json())
-        .then(data => {
-          let joke = data.joke;
-          jokeBox.textContent = joke;
-          // Open
-          curtains.forEach(bar => {
-            bar.classList.add("open");
-            bar.classList.remove("shut");
-          });
+function action() {
+  // Shut
+  curtains.forEach(bar => {
+    bar.classList.add("shut");
+    bar.classList.remove("open");
+  });
+  // Fetch
+  window.setTimeout(function() {
+    fetch("https://icanhazdadjoke.com/", {
+      headers: { Accept: "application/json" }
+    })
+      .then(res => res.json())
+      .then(data => {
+        let joke = data.joke;
+        jokeBox.textContent = joke;
+        // Open
+        curtains.forEach(bar => {
+          bar.classList.add("open");
+          bar.classList.remove("shut");
         });
-    }, 2000);
+      });
+  }, 2000);
+}
+
+window.addEventListener("keypress", e => {
+  if (e.keyCode === 32) {
+    action();
   }
+});
+
+window.addEventListener("touchstart", e => {
+  action();
 });
